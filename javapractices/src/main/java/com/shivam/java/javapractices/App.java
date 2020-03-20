23729af3a1f92ea95012f45efb87013c1129bf08
Shivam Jari <sjari@paypal.com>
2020-03-20 14:52:53 +0530

    adding code

---
+++ javapractices/src/main/java/com/shivam/java/javapractices/App.java
@@ -0,0 +1,13 @@
+package com.shivam.java.javapractices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
